82aa0fa5cd5ac2da60f5d1821b50a2104b4a1791
wangw571 <wang_w571@hotmail.com>
2016-05-24 17:58:43 -0400

    Looks Much More better

--- Mod.cs
+++ Mod.cs
@@ -9,7 +9,7 @@ namespace Blocks
 {
     public class CloakingDeviceMod : BlockMod
     {
-        public override Version Version { get { return new Version("1.8"); } }
+        public override Version Version { get { return new Version("1.9"); } }
         public override string Name { get { return "Cloaking_Device_Mod"; } }
         public override string DisplayName { get { return "Cloaking Device Mod"; } }
         public override string BesiegeVersion { get { return "v0.27"; } }
@@ -22,15 +22,15 @@ namespace Blocks
             .BlockName("Improved Cloaking Device​")
 
             ///模型信息
-            .Obj(new List<Obj> { new Obj("turret.obj", //Obj
-                                         "Cormack\'s Modified Tracking Computer​.png", //贴图
-                                         new VisualOffset(new Vector3(0.01f, 0.01f, 0.01f), //Scale
+            .Obj(new List<Obj> { new Obj("Cloak Device.obj", //Obj
+                                         "Cloak Device Texture.png", //贴图
+                                         new VisualOffset(new Vector3(1f, 1f, 1f), //Scale
                                                           new Vector3(0f, 0f, 0f), //Position
                                                           new Vector3(0f, 0f, 0f)))//Rotation
             })
 
             ///在UI下方的选模块时的模样
-            .IconOffset(new Icon(new Vector3(1.30f, 1.30f, 1.30f) * 0.06f,  //Scale
+            .IconOffset(new Icon(new Vector3(1.30f, 1.30f, 1.30f),  //Scale
                                  new Vector3(-0.11f, -0.13f, 0.00f),  //Position
                                  new Vector3(85f, 90f, 270f))) //Rotation
 
@@ -50,34 +50,12 @@ namespace Blocks
             .Mass(2f)
 
             ///是否显示碰撞器（在公开你的模块的时候记得写false）
-            .ShowCollider(true)
+            .ShowCollider(false)
 
             ///碰撞器
             .CompoundCollider(new List<ColliderComposite> {
-                ColliderComposite.Box(new Vector3(0.7f, 0.7f, 1.3f), new Vector3(0f, 0f, 0.8f), new Vector3(0f, 0f, 0f)),
-                ColliderComposite.Capsule(0.35f,1.0f,Direction.Z,new Vector3(0,0,0.8f),Vector3.zero),/*
-                                ColliderComposite.Sphere(0.49f,                                //radius
-                                                         new Vector3(-0.10f, -0.05f, 0.27f),   //Position
-                                                         new Vector3(0f, 0f, 0f))              //Rotation
-                                                         .IgnoreForGhost(),                    //Do not use this collider on the ghost
-
-                                ColliderComposite.Capsule(0.33f,                               //radius
-                                                          1.33f,                               //length
-                                                          Direction.Y,                         //direction
-                                                          new Vector3(-0.52f, 0.38f, 0.30f),   //position
-                                                          new Vector3(5f, 0f, -5f)),           //rotation                                
-                                
-                                ColliderComposite.Box(new Vector3(0.65f, 0.65f, 0.25f),        //scale
-                                                      new Vector3(0f, 0f, 0.25f),              //position
-                                                      new Vector3(0f, 0f, 0f)),                //rotation
-                                
-                                ColliderComposite.Sphere(0.5f,                                  //radius
-                                                         new Vector3(-0.10f, -0.05f, 0.35f),    //Position
-                                                         new Vector3(0f, 0f, 0f))               //Rotation
-                                                         .Trigger().Layer(2)
-                                                         .IgnoreForGhost(),                     //Do not use this collider on the ghost
-                              //ColliderComposite.Box(new Vector3(0.35f, 0.35f, 0.15f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f)).Trigger().Layer(2).IgnoreForGhost(),   <---Example: Box Trigger on specific Layer*/
-            })
+                ColliderComposite.Mesh("Cloak Device.obj",Vector3.one,Vector3.zero,new Vector3(90,0,0))
+                                })
 
             ///你的模块是不是可以忽视强搭
             .IgnoreIntersectionForBase()
@@ -93,15 +71,8 @@ namespace Blocks
             ///连接点
             .AddingPoints(new List<AddingPoint> {
                                (AddingPoint)new BasePoint(true, true)         //底部连接点。第一个是指你能不能将其他模块安在该模块底部。第二个是指这个点是否是在开局时粘连其他链接点
-                                                .Motionable(true,true,true) //底点在X，Y，Z轴上是否是能够活动的。
+                                                .Motionable(false,false,false) //底点在X，Y，Z轴上是否是能够活动的。
                                                 .SetStickyRadius(0.5f),        //粘连距离
-                              new AddingPoint(new Vector3(0f, 0f, 1f), new Vector3(-180f, 00f, 360f),true).SetStickyRadius(0.3f),
-                              new AddingPoint(new Vector3(0f, 0f, 1f), new Vector3(-90f, 00f, 90f),true).SetStickyRadius(0.3f),
-                              new AddingPoint(new Vector3(0f, 0f, 1f), new Vector3(180f, 00f, 180f),true).SetStickyRadius(0.3f),
-                              new AddingPoint(new Vector3(0f, 0f, 1f), new Vector3(90f, 00f, 270f),true).SetStickyRadius(0.3f),
-
-
-                              new AddingPoint(new Vector3(0f, 0f, 1f), new Vector3(0f, -90f, 90f),true).SetStickyRadius(0.3f),
             });
 
         public override void OnLoad()
@@ -125,6 +96,7 @@ namespace Blocks
         private bool Activated;
         private float progress;
         private float ReactivationTimer;
+        private Material InvBumpTex;
         private Material InvTex;
         private float CoolDownTime;
         public float 记录器 = 0;
@@ -173,16 +145,23 @@ namespace Blocks
             DecloakAudio.volume = 1f;
             FieldSize = Range.Value;
             CoolDownTime = Cooldown.Value;
-            InvTex = new Material(Shader.Find("FX/Glass/Stained BumpDistort"));
-            //InvTex = new Material(Shader.Find("Transparent/Diffuse"));
+            InvBumpTex = new Material(Shader.Find("FX/Glass/Stained BumpDistort"));
+            
+            InvBumpTex.color = new Color(1, 1, 1, 0);
+            InvBumpTex.mainTexture = resources["Speed_Bump.png"].texture;
+            InvBumpTex.SetTexture("_BumpMap", resources["Speed_Bump.png"].texture);
+
+            InvTex = new Material(Shader.Find("Transparent/Diffuse"));
             InvTex.color = new Color(1, 1, 1, 0);
-            InvTex.mainTexture = resources["Speed_Bump.png"].texture;
-            InvTex.SetTexture("_BumpMap", resources["Speed_Bump.png"].texture);
+            InvTex.mainTexture = resources["ALL BLACK.jpg"].texture;
+
             foreach (MeshRenderer REDer in Machine.Active().SimulationMachine.GetComponentsInChildren<MeshRenderer>())
                 {
                     if (!REDer.GetComponent<BeingCloakedScript>())
                     {
                     REDer.gameObject.AddComponent<BeingCloakedScript>();
+                    REDer.gameObject.GetComponent<BeingCloakedScript>().InvisibleBumpedMat = (InvBumpTex);
+                    REDer.gameObject.GetComponent<BeingCloakedScript>().InvisibleBumpedMat.mainTexture = (InvBumpTex.mainTexture);
                     REDer.gameObject.GetComponent<BeingCloakedScript>().InvisibleMat = (InvTex);
                     REDer.gameObject.GetComponent<BeingCloakedScript>().InvisibleMat.mainTexture = (InvTex.mainTexture);
                     REDer.gameObject.GetComponent<BeingCloakedScript>().duration = Speed.Value;
@@ -217,21 +196,24 @@ namespace Blocks
                     this.transform.localScale.x)
                     * FieldSize * Mathf.Min(ReactivationTimer, Speed.Value) / Speed.Value;
             }
-
-            foreach (BeingCloakedScript BCS in FindObjectsOfType<BeingCloakedScript>())
+            if (Activated || ReactivationTimer < Speed.Value)
             {
-                if ((BCS.MyPos - this.transform.position).sqrMagnitude < size * size)
+                foreach (BeingCloakedScript BCS in FindObjectsOfType<BeingCloakedScript>())
                 {
-                    progress += Time.fixedDeltaTime / 3;
-                    BCS.InsideAField = true;
+                    if ((BCS.MyPos - this.transform.position).sqrMagnitude < size * size)
+                    {
+                        progress += Time.fixedDeltaTime / 3;
+                        BCS.InsideAField = true;
+                    }
                 }
+            Debug.Log(Activated + " " + ReactivationTimer);
             }
-            //Debug.Log(Activated + " " + size);
         }
     }
     public class BeingCloakedScript:MonoBehaviour
     {
         private Material OriginalMat;
+        public Material InvisibleBumpedMat;
         public Material InvisibleMat;
         private Color OrigColor = new Color();
         public float duration = 3;
@@ -240,6 +222,7 @@ namespace Blocks
         public float 渐变数值;
         public Vector2 BumpOFFSET;
         public Vector2 BumpOFFSET惯性;
+        private string Oldname;
         void Start()
         {
             MyPos = this.transform.position;
@@ -252,6 +235,8 @@ namespace Blocks
             //GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0);
             BumpOFFSET = Vector2.zero;
             BumpOFFSET惯性 = Vector2.zero;
+            Oldname = this.GetComponentInParent<MachineTrackerMyId>().gameObject.name;
+            DontDestroyOnLoad(InvisibleBumpedMat);
             DontDestroyOnLoad(InvisibleMat);
             DontDestroyOnLoad(OriginalMat);
         }
@@ -267,18 +252,43 @@ namespace Blocks
             if(InsideAField)
             {
                 Cloaking(渐变数值 * 2, OrigColor, BumpOFFSET);
+                if (!this.GetComponentInParent<MachineTrackerMyId>().gameObject.name.Contains("Cloaked"))
+                    this.GetComponentInParent<MachineTrackerMyId>().gameObject.name += "Cloaked";
             }
             else
             {
                 Decloaking(渐变数值 * 2,OrigColor);
+                if (this.GetComponentInParent<MachineTrackerMyId>().gameObject.name.Contains("Cloaked"))
+                    this.GetComponentInParent<MachineTrackerMyId>().gameObject.name = Oldname;
             }
             InsideAField = false;
         }
         void Decloaking(float ProgressTimesBy2, Color OriginalColor)
         {
             ProgressTimesBy2 = Mathf.Clamp(ProgressTimesBy2, 0, 2);
-            if (ProgressTimesBy2 > 1)
-                GetComponent<Renderer>().material.color = Color.white * (ProgressTimesBy2 - 1);
+
+
+            if (ProgressTimesBy2 > 1.5f)
+            {
+                GetComponent<Renderer>().material.SetColor("_BumpMap", Color.white * (ProgressTimesBy2 - 1.5f) * 4);
+                if (GetComponent<Renderer>().material.shader != InvisibleBumpedMat.shader)
+                {
+                    GetComponent<Renderer>().material.shader = InvisibleBumpedMat.shader;
+                    GetComponent<Renderer>().material.mainTexture = null;
+                    GetComponent<Renderer>().material.SetTexture("_BumpMap", InvisibleBumpedMat.GetTexture("_BumpMap"));
+                }
+            }
+            else if (ProgressTimesBy2 > 1f)
+            {
+                GetComponent<Renderer>().material.color = Color.white * (1.5f - ProgressTimesBy2) * 4;
+                if (GetComponent<Renderer>().material.shader != InvisibleMat.shader)
+                {
+                    GetComponent<Renderer>().material.shader = InvisibleMat.shader;
+                    GetComponent<Renderer>().material.mainTexture = InvisibleMat.mainTexture;
+                    GetComponent<Renderer>().material.SetTexture("_BumpMap", InvisibleBumpedMat.mainTexture);
+                }
+            }
+
             else if (ProgressTimesBy2 > 0)
             {
                 GetComponent<Renderer>().material.color = OriginalColor * (1 - ProgressTimesBy2);
@@ -289,29 +299,48 @@ namespace Blocks
                     GetComponent<Renderer>().material.SetTexture("_BumpMap", null);
                 }
             }
+
+
+            /*if (ProgressTimesBy2 > 1)
+                GetComponent<Renderer>().material.color = Color.white * (ProgressTimesBy2 - 1);
+            else if (ProgressTimesBy2 > 0)
+            {
+                GetComponent<Renderer>().material.color = OriginalColor * (1 - ProgressTimesBy2);
+                if (GetComponent<Renderer>().material.shader != OriginalMat.shader)
+                {
+                    GetComponent<Renderer>().material.shader = OriginalMat.shader;
+                    GetComponent<Renderer>().material.mainTexture = OriginalMat.mainTexture;
+                    GetComponent<Renderer>().material.SetTexture("_BumpMap", null);
+                }
+            }*/
         }
         void Cloaking(float ProgressTimesBy2, Color OriginalColor, Vector2 BumpMapOffSet)
         {
             if (ProgressTimesBy2 < 1)
                 GetComponent<Renderer>().material.color = OriginalColor * (1 - ProgressTimesBy2);
-            else if (ProgressTimesBy2 < 2)
+            else if (ProgressTimesBy2 < 1.5f)
             {
-                GetComponent<Renderer>().material.color = Color.white * ( ProgressTimesBy2 - 1);
-                if(GetComponent<Renderer>().material.shader != InvisibleMat.shader)
+                GetComponent<Renderer>().material.color = Color.white * (1.5f - ProgressTimesBy2) * 4;
+                if (GetComponent<Renderer>().material.shader != InvisibleMat.shader)
                 {
                     GetComponent<Renderer>().material.shader = InvisibleMat.shader;
+                    GetComponent<Renderer>().material.mainTexture = InvisibleMat.mainTexture;
+                    GetComponent<Renderer>().material.SetTexture("_BumpMap", InvisibleBumpedMat.mainTexture);
+                }
+            }
+            else if (ProgressTimesBy2 < 2f)
+            {
+                GetComponent<Renderer>().material.SetColor("_BumpMap", Color.white * (1.5f - ProgressTimesBy2) * 4);
+                if (GetComponent<Renderer>().material.shader != InvisibleBumpedMat.shader)
+                {
+                    GetComponent<Renderer>().material.shader = InvisibleBumpedMat.shader;
                     GetComponent<Renderer>().material.mainTexture = null;
-                    GetComponent<Renderer>().material.SetTexture("_BumpMap", InvisibleMat.mainTexture);
+                    GetComponent<Renderer>().material.SetTexture("_BumpMap", InvisibleBumpedMat.GetTexture("_BumpMap"));
                 }
             }
             GetComponent<Renderer>().material.SetTextureOffset("_BumpMap", BumpMapOffSet);
         }
     }
-    public class CloakFieldScript : MonoBehaviour
-    {
-        public Queue<List<float>> GeneratorPositions = new Queue<List<float>>();
-    }
-    //Physics stuff
 }
 
 
